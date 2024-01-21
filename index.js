c4c11abd378e7078bdfa47ee98bf4c74fe2ceba1
mike-degreat <okoronum@gmail.com>
2024-01-21 12:45:01 +0100

    function to display menu

---
+++ index.js
@@ -0,0 +1,10 @@
+// function to display menu
+function displayMenu() {
+    console.clear();
+    console.log("** Welcome to the E-commerce Console App **");
+    console.log("1. Browse Products");
+    console.log('2. Add to Cart');
+    console.log("3. View Cart");
+    console.log("4. Checkout");
+    console.log("5. Exit");
+}
